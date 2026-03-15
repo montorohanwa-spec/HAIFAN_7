f4d3d9689a8f882578b6ef2f228024df78d37f8b
montorohanwa-spec <montorohanwa@gmail.com>
2026-03-15 12:28:41 +0100

    Add files via upload

---
+++ ad.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from groq import Groq
+
+st.title("General Knowledge Bot")
+
+st.caption("Ask me absolutely anything!")
+
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+
+system_prompt = "You are a highly intelligent,friendly and helpful AI assistant.Answer any question the user asks clearly and accurately."
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "system", "content": system_prompt}]
+
+for msg in st.session_state.messages:
+    if msg["role"] != "system":
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
+            
+if prompt := st.chat_input("Ask me anything!"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    answer = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=st.session_state.messages
+    ).choices[0].message.content
+    
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+    with st.chat_message("assistant"):
+        st.markdown(answer)
